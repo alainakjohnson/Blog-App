be0b068703fd73c2b57965cb7bdb83ae5097fecf
alainakjohnson <alainakjohnson@gmail.com>
2018-10-16 02:28:12 +0000

    form added

--- src/containers/posts_new.js
+++ src/containers/posts_new.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Field, reduxForm, formValueSelector } from "redux-form";
 
 class PostsNew extends Component {
+    renderTitleField(field){
+        return(
+            <div className="form-group">
+                <label>Title</label>
+                <input
+                    {...field.input}
+                    className="form-control"
+                    type="text"
+                />
+            </div>
+        )
+    }
+    
+    onSubmit(values){
+        console.log(values);
+    }
+        
     render() {
+        const { handleSubmit } = this.props;
         return (
-            <div>
-                PostsNew
+            <div className="container">
+                <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                    <Field name="title" component={this.renderTitleField} />
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
             </div>
         );
     }
 }
+    
+    
+PostsNew = reduxForm({
+    form: "PostsNewForm"
+})(PostsNew);
+
+const selector = formValueSelector('PostsNewForm');
+PostsNew = connect(state => {
+  // can select values individually
+  const title = selector(state, 'title')
+
+  // props
+  return {
+    title,
+  }
+})(PostsNew);
 
 export default PostsNew;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 //reducer index
 import PostsReducer from "./reducer_posts";
+import { reducer as formReducer } from 'redux-form';
 import {combineReducers} from 'redux';
 
 const rootReducer = combineReducers({
-    posts: PostsReducer
+    posts: PostsReducer,
+    form: formReducer
 });
 
 export default rootReducer;
