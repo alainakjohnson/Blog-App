3a5246435835129fee3b13dd14bc842b15848e18
alainakjohnson <alainakjohnson@gmail.com>
2018-10-16 03:47:35 +0000

    create post action added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = "https://blog-server-alaina-johnson.herokuapp.com/api";
 
@@ -9,6 +10,15 @@ export function fetchPosts(){
 
     return ({
         type: FETCH_POSTS,
+        payload: request
+    })
+}
+
+export function createPost(values){
+    const request = axios.post(`${ROOT_URL}/posts`, values);
+
+    return ({
+        type: CREATE_POST,
         payload: request
     })
 }
